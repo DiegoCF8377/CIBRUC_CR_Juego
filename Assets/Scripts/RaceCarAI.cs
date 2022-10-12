ba20d5798b21bc97d00572c988d4a28c86806665
Chris L <chris.rey.lop.g@gmail.com>
2022-10-11 20:09:25 -0500

    Update Mapa 2

---
+++ Assets/Scripts/RaceCarAI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaceCarAI : MonoBehaviour
+{
+    public Transform[] trackCourse;
+    private Rigidbody raceCar;
+    private Transform carDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        raceCar = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        raceCar.velocity = transform.forward * 15.0f;
+    }
+}
