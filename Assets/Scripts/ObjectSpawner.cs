6b123083518d4f78a50cbb9a6daf6f0393a1567f
zLuis <a01705457@itesm.mx>
2023-09-12 20:18:46 -0600

    Revamp del sistema de generación de objetos
    Adiciones:
    - Se reestructura el código para la generación de objetos dentro de arrays para controlar su orden de spawn.
    - Se hace un chequeo que forzosamente causa que haya al menos una basura organica para el jugador.
    - Se reorganiza la manera en la que se spawnean los objetos, en vez de crear objetos y hacer que sean visibles, unicamente tomamos el spawner y cada que se renderiza toma una textura aleatoria correspondiente a cada uno de los 3 tipos de basura.
    - Se implementa una seccion en el codigo para agregar aquellos objetos que querramos agregar por su nombre, estos archivos deben de estar guardados en la nueva carpeta llamada Resources dentro de Assets, en el mismo archivo de ModelSelector puedes cambiar la rotacion y la escala del objeto individualmente.
    - Se pasa la lógica de colision y manejo de objetos completamente al archivo de los objetos y lo sacamos de lo que es controlado por el jugador haciendo la estructura mas ordenada.
    - Se agregaron nuevas basuras para cada tipo de basura (esto es para testeo pero se pueden agregar o borrar la cantidad de modelos que se deseen, solo notar que deben estar dentro de Resources).
    
    Notas:
    - Durante las pruebas parece que en ocaciones se frecuenta mas de lo que deberia el spawn de las basuras organicas.
    - Por las curvas y rotaciones del ambiente, algunos objetos parecen rotados sobre su eje.
    - Algunas texturas originales son demasiado grandes y deben modificarse en los scripts.
    - Por la forma en la que trabaja unity, fui incapaz de encontrar una manera de crear carpetas para mantener todo más ordenado.

--- Assets/Scripts/ObjectSpawner.cs
+++ Assets/Scripts/ObjectSpawner.cs
@@ -2,72 +2,100 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectSpawner : MonoBehaviour
+public enum TrashType
+    {
+        OrganicTrash,
+        NonOrganicTrash,
+        ElectronicTrash
+    }
+
+public class ObjectSpawner : MonoBehaviour 
 {
-    public GameObject organicTrash;
+    // Declare the 3 types of trash that will be spawned
+    public GameObject organicTrash; // Only trash that provides fuel
     public GameObject nonOrganicTrash;
     public GameObject electronicTrash;
+    public bool isOrganicSpawned = false;
+    public SpawnTracker[] trashArray = {
+                                        new SpawnTracker {isSpawned = false},
+                                        new SpawnTracker {isSpawned = false},
+                                        new SpawnTracker {isSpawned = false}
+                                       };
+    
 
-    public bool spawnOrganic = false;
-    public bool spawnNonOrganic = false;
-    public bool spawnElectronic = false;
-
-    public float trashTimer = 10.0f;
-    private float trashTimerLimit;
-
-    public void setTrashActive()
+    private void setTrashActive()
     {
-        if(spawnOrganic && organicTrash.activeInHierarchy == false)
+        // int i = 0;
+        foreach (SpawnTracker trashSpawner in trashArray)
         {
-            if(trashTimer <= 0)
+            if (trashSpawner.objectReference == null)
             {
-                organicTrash.SetActive(true);
-                organicTrash.GetComponent<RandomModelSelector>().deactivateAllTrashModels();
-                organicTrash.GetComponent<RandomModelSelector>().selectRandomModel(Random.Range(0,3));
-                trashTimer = trashTimerLimit;
+                continue;
             }
-            else if(trashTimer > 0)
+            // Debug.Log("TrashSpawner: " + trashSpawner.type + " " + " " + trashSpawner.objectReference.activeInHierarchy + " " + trashSpawner.trashTimer + " " + i);
+            // i++;
+            if (trashSpawner.trashTimer > 0 && !trashSpawner.objectReference.activeSelf) 
             {
-                trashTimer -= Time.deltaTime;
+                trashSpawner.trashTimer -= Time.deltaTime;
+                if (isOrganicSpawned && trashSpawner.type == TrashType.OrganicTrash)
+                {
+                    isOrganicSpawned = false;
+                }
+                continue;
             }
-        }
-        else if(spawnNonOrganic && nonOrganicTrash.activeInHierarchy == false)
-        {
-            if(trashTimer <= 0)
+            if (!trashSpawner.objectReference.activeSelf)
             {
-                nonOrganicTrash.SetActive(true);
-                nonOrganicTrash.GetComponent<RandomModelSelector>().deactivateAllTrashModels();
-                nonOrganicTrash.GetComponent<RandomModelSelector>().selectRandomModel(Random.Range(0,3));
-                trashTimer = trashTimerLimit;
-            }
-            else if(trashTimer > 0)
-            {
-                trashTimer -= Time.deltaTime;
-            }
-        }
-        else if(spawnElectronic && electronicTrash.activeInHierarchy == false)
-        {
-            if(trashTimer <= 0)
-            {
-                electronicTrash.SetActive(true);
-                electronicTrash.GetComponent<RandomModelSelector>().deactivateAllTrashModels();
-                electronicTrash.GetComponent<RandomModelSelector>().selectRandomModel(Random.Range(0,3));
-                trashTimer = trashTimerLimit;
-            }
-            else if(trashTimer > 0)
-            {
-                trashTimer -= Time.deltaTime;
+                trashSpawner.isSpawned = true;
+                if (!isOrganicSpawned)
+                {
+                    isOrganicSpawned = true;
+                    trashSpawner.type = TrashType.OrganicTrash;
+                } else 
+                {
+                    trashSpawner.type = Random.Range(0, 2) == 0 ? TrashType.NonOrganicTrash : TrashType.ElectronicTrash;
+                }
+                trashSpawner.objectReference.GetComponent<RandomModelSelector>().selectRandomModel(trashSpawner.type.ToString());
+                trashSpawner.trashTimer = 3.0f;
+                switch (trashSpawner.type)
+                {
+                    case TrashType.OrganicTrash:   
+                        trashSpawner.objectReference.tag = "OrganicTrash";
+                        break;
+                    case TrashType.NonOrganicTrash:
+                        trashSpawner.objectReference.tag = "NonOrganicTrash";
+                        break;
+                    case TrashType.ElectronicTrash:
+                        trashSpawner.objectReference.tag = "ElectronicTrash";
+                        break;
+                }
+                trashSpawner.objectReference.SetActive(true);
             }
         }
     }
 
     private void Update()
     {
-        setTrashActive();  
+        setTrashActive();
     }
 
     private void Start()
     {
-        trashTimerLimit = trashTimer;
+        trashArray[0].objectReference = organicTrash;
+        organicTrash.GetComponent<RandomModelSelector>().selectRandomModel("OrganicTrash");
+        trashArray[1].objectReference = nonOrganicTrash;
+        nonOrganicTrash.GetComponent<RandomModelSelector>().selectRandomModel("NonOrganicTrash");
+        trashArray[2].objectReference = electronicTrash;
+        electronicTrash.GetComponent<RandomModelSelector>().selectRandomModel("ElectronicTrash");
     }
+
 }
+
+public class SpawnTracker
+{
+    public bool isSpawned { get; set; }
+    public TrashType type { get; set; }
+    public GameObject? objectReference { get; set; }
+    public float trashTimer = 0;
+    
+}
+
--- Assets/Scripts/RandomModelSelector.cs
+++ Assets/Scripts/RandomModelSelector.cs
@@ -4,16 +4,103 @@ using UnityEngine;
 
 public class RandomModelSelector : MonoBehaviour
 {
-    public GameObject [] trashModels = new GameObject[3];
+    public GameObject currentActiveModel;
+    public Model[] organicPreFabs = {
+                                    new Model {modelName = "appleHalf",
+                                                            // new Quaternion(Y, X, Z, W)
+                                               modelRotation = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f),
+                                               modelScale = new Vector3(1.0f, 1.0f, 1.0f)},
+                                    new Model {modelName = "bacon",
+                                               modelRotation = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f),
+                                               modelScale = new Vector3(3.0f, 3.0f, 3.0f)},
+                                    new Model {modelName = "burger",
+                                               modelRotation = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f),
+                                               modelScale = new Vector3(2.0f, 2.0f, 2.0f)},
+                                    new Model {modelName = "avocadoHalf",
+                                               modelRotation = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f),
+                                               modelScale = new Vector3(1.0f, 1.0f, 1.0f)},
+                                    new Model {modelName = "bowlCereal",
+                                               modelRotation = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f),
+                                               modelScale = new Vector3(1.0f, 1.0f, 1.0f)}
+                                    };
+    public Model[] nonOrganicPreFabs = {
+                                        new Model {modelName = "bag",
+                                                   modelRotation = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f),
+                                                   modelScale = new Vector3(1.0f, 1.0f, 1.0f)},
+                                        new Model {modelName = "chopstickFancy",
+                                                   modelRotation = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f),
+                                                   modelScale = new Vector3(3.0f, 3.0f, 3.0f)},
+                                        new Model {modelName = "cup", 
+                                                   modelRotation = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f), 
+                                                   modelScale = new Vector3(2.5f, 2.5f, 2.5f)},
+                                        new Model {modelName = "styrofoam", 
+                                                   modelRotation = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f), 
+                                                   modelScale = new Vector3(-1.0f, 1.0f, 1.0f)},
+                                        new Model {modelName = "fryingPan", 
+                                                   modelRotation = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f), 
+                                                   modelScale = new Vector3(1.0f, 1.0f, 1.0f)}
+                                        };
+    public Model[] electronicPreFabs = {
+                                        new Model {modelName = "radio", 
+                                                   modelRotation = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f), // Currently rotated to the right
+                                                   modelScale = new Vector3(1.0f, 1.0f, 1.0f)},
+                                        new Model {modelName = "laptop", 
+                                                   modelRotation = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f), 
+                                                   modelScale = new Vector3(0.1f, 0.1f, -0.1f)},
+                                        new Model {modelName = "speakerSmall", 
+                                                   modelRotation = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f), 
+                                                   modelScale = new Vector3(0.3f, 0.3f, 0.3f)},
+                                        new Model {modelName = "computerScreen", 
+                                                   modelRotation = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f), // Currently rotated to the right
+                                                   modelScale = new Vector3(0.3f, 0.3f, -0.3f)},
+                                        new Model {modelName = "dryer", 
+                                                   modelRotation = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f), 
+                                                   modelScale = new Vector3(0.2f, 0.2f, -0.2f)}
+                                        };
 
-    public void selectRandomModel(int randomIndex)
+    public void selectRandomModel(string modelType)
     {
-        trashModels[randomIndex].SetActive(true);
+        deactivateAllTrashModels();
+        Model model = new Model();
+        switch (modelType)
+        {
+            case "OrganicTrash":
+                model = organicPreFabs[Random.Range(1, 5)];
+                break;
+            case "NonOrganicTrash":
+                model = nonOrganicPreFabs[Random.Range(1, 5)];
+                break;
+            case "ElectronicTrash":
+                model = electronicPreFabs[Random.Range(1, 5)];
+                break;
+        }
+        GameObject myPrefab = Resources.Load<GameObject>(model.modelName);
+        currentActiveModel = Instantiate(myPrefab);
+        currentActiveModel.transform.position = this.transform.position;
+        currentActiveModel.transform.rotation = model.modelRotation;
+        currentActiveModel.transform.localScale = model.modelScale;
     }
+
+    public class Model
+    {
+        public string modelName { get; set; }
+        public Quaternion modelRotation { get; set; }
+        public Vector3 modelScale { get; set; }
+
+    }
+
     public void deactivateAllTrashModels()
     {
-        trashModels[0].SetActive(false);
-        trashModels[1].SetActive(false);
-        trashModels[2].SetActive(false);
+        if (currentActiveModel == null)
+        {
+            return;
+        }
+        currentActiveModel.SetActive(false);
+        Destroy(currentActiveModel);
+    }
+
+    private void OnTriggerEnter(Collider coll)
+    {
+        deactivateAllTrashModels();
     }
 }
